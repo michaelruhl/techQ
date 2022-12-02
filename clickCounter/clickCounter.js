724c685da641994b2c90e780b90af8f5b6b28b4a
Michael Ruhl <michaelruhl14@hotmail.com>
2022-12-02 15:36:26 -0500

    click counter

---
+++ clickCounter/clickCounter.js
@@ -0,0 +1,8 @@
+// ## MY JAVASCRIPT
+let counter = document.getElementById("counter")
+let result = document.getElementById("result")
+let count = 0
+counter.addEventListener("click", () => {
+  count += 1
+  result.innerHTML = count
+})
